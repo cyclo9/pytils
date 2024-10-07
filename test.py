b2ed3952c1f9b1745ec21c4ca9842143a51738fa
cyclo9 <32120227+cyclo9@users.noreply.github.com>
2024-10-06 23:08:53 -0700

    sync

--- test.py
+++ test.py
@@ -1,26 +1,54 @@
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning)
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
 from models import *
-import torch
+import gymnasium as gym
+from rlutils import *
+# from tensordict import TensorDict
+# from torchrl.data import ReplayBuffer, LazyTensorStorage
 
-# Define parameters
-batch_size = 2
-seq_len_src = 10
-seq_len_tgt = 8
-d_model = 16  # Number of features (dimensions)
-n_features = 5  # Output features
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# Create random source and target sequences with the same batch size
-src = torch.randn(batch_size, seq_len_src, d_model)  # Source sequence
-tgt = torch.randn(batch_size, seq_len_tgt, d_model)  # Target sequence
+in_features = 2
+out_features = 3
+n_layers = 2
+n_units = 64
 
-# Create the model
-model = TransformerModel(
-    d_model=d_model, out_features=n_features, n_layers=2, dropout=0.1
-)
+actor_net = FeedForward(in_features, out_features, n_layers, n_units)
+value_net = FeedForward(in_features, 1, n_layers, n_units)
 
-# Perform a forward pass
-output = model(src, tgt)
+actor = StochasticActor(actor_net, True)
+env = gym.make("CartPole-v1", render_mode='human')
 
-print("Source Shape:", src.shape)
-print("Target Shape:", tgt.shape)
-print("Output Shape:", output.shape)
-print(output)
+buffer = Collector()
+gae = GAE(gamma=0.99, lmbda=0.95, value_net=value_net)
+
+for i in range(200):
+    state = env.reset()
+    state = torch.tensor(state[0], dtype=torch.float, device=device)
+    action, log_prob, entropy = actor(state)
+
+    next_state, reward, terminated, truncated, info = env.step(action.item())
+    done = terminated or truncated
+
+    transition = {
+        "state": state, 
+        "next_state": torch.tensor(next_state, dtype=torch.float, device=device),
+        "reward": torch.tensor([reward], dtype=torch.float, device=device),
+        "done": torch.tensor([float(terminated or truncated)], dtype=torch.float, device=device)
+    }
+
+    buffer.add(transition)
+
+    if done:
+        break
+
+states = buffer["state"]
+next_states = buffer["next_state"]
+rewards = buffer["reward"]
+dones = buffer["done"]
+
+advantages, returns = gae(states, next_states, rewards, dones)
+
+print(advantages)
